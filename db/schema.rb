b31950a0855b2ff88019b526466714d2a3677ec2
ROBERT Nishimwe <nzeroberto@gmail.com>
2019-06-01 11:18:14 +0000

    step4manyochallenge

---
+++ db/schema.rb
@@ -0,0 +1,40 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2019_06_01_105810) do
+
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "windows"
+    t.string "mac"
+    t.string "linux"
+    t.string "android"
+    t.string "iphone"
+    t.string "hardware"
+    t.string "software"
+    t.text "name"
+    t.text "content"
+    t.integer "blog_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "encrypted_password"
+    t.string "password_digest"
+  end
+
+end
